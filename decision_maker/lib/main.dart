4b81c8e5a7f2258b8fa01babbdf5074c254ddb03
Okashe <okashabdi88@gmail.com>
2023-05-29 17:08:04 -0500

    challenge complete

--- decision_maker/lib/main.dart
+++ decision_maker/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,12 +15,42 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 78, 169, 243),
+      backgroundColor: const Color.fromARGB(255, 78, 169, 243),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 26, 47, 154),
+        backgroundColor: const Color.fromARGB(255, 26, 47, 154),
         title: const Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+  void randomAnswer() {
+    setState(() {
+      answer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          randomAnswer();
+        },
+        child: Image(
+          image: AssetImage('images/ball$answer.png'),
+        ),
+      ),
     );
   }
 }
